163a8b84b1b8156937244852b5f193806227d23f
Lafif Astahdziq <hello@lafif.me>
2024-01-02 22:12:07 +0700

    build: add static build config

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,7 @@ import { unifiedConditional } from 'unified-conditional'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
